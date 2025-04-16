34cc3a5916065a21300e46ffc1db46a1969126ff
faroutnerd <farhan91179@gmail.com>
2025-04-16 17:58:45 +0530

    port

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server running on http://localhost:${PORT}`);
